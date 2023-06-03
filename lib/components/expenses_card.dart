b6f4bd2580bc95e2ed73b92a7a737a90f697a49a
Saif64 <saifmahmud81@gmail.com>
2023-06-03 23:49:56 +0600

    added favourite icon toggle

--- lib/components/expenses_card.dart
+++ lib/components/expenses_card.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:personal_expenses/model/expenses_model.dart';
 
-class ExpenseCard extends StatelessWidget {
+class ExpenseCard extends StatefulWidget {
   const ExpenseCard(this.expense, {super.key});
 
   final Expense expense;
+
+  @override
+  State<ExpenseCard> createState() => _ExpenseCardState();
+}
+
+class _ExpenseCardState extends State<ExpenseCard> {
+  bool isPressedIcon = false;
+
+  void addToFav() {
+    setState(() {
+      isPressedIcon = !isPressedIcon;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,23 +31,36 @@ class ExpenseCard extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              expense.name,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  widget.expense.name,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                IconButton(
+                    onPressed: addToFav,
+                    icon: isPressedIcon == true
+                        ? const Icon(Icons.favorite)
+                        : const Icon(Icons.favorite_border))
+              ],
+            ),
+            const SizedBox(
+              height: 5,
             ),
             Row(
               children: [
-                Text('${expense.amount.toStringAsFixed(2)} BDT'),
+                Text('${widget.expense.amount.toStringAsFixed(2)} BDT'),
                 const Spacer(),
                 Row(
                   children: [
-                    Icon(iconBasedOnCategory[expense.category]),
+                    Icon(iconBasedOnCategory[widget.expense.category]),
                     const SizedBox(width: 10),
                     Text(
-                      expense.formattedTime,
+                      widget.expense.formattedTime,
                     ),
                   ],
                 )
